de005c9925e084016f6fb8478b1eb31d93687e50
hrz07 <hridoy.developer@yahoo.com>
2022-05-02 01:38:15 +0600

    inventory card desined

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import img from '../../images/man.png'
+import InventoryCard from '../InventoryCard/InventoryCard';
 import './Home.css';
 
 const Home = () => {
+
+    const [items,setItems] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:4000/items')
+        .then(res => res.json())
+       .then(data=> setItems(data)) 
+  },[items])
+
+
+
+
     return (
         <div>
             <section className='banner'>
@@ -15,6 +27,12 @@ const Home = () => {
                 <div className='imgSection'>
                     <img src={img} alt="" />
                 </div>
+            </section>
+
+            <section className='inventory'>
+                {
+                    items.map(item=> <InventoryCard  key={item._id} item={item} /> )
+                }
             </section>
         </div>
     );
--- src/Components/InventoryCard/InventoryCard.js
+++ src/Components/InventoryCard/InventoryCard.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './InventoryCard.css'
 
-const InventoryCard = () => {
+const InventoryCard = ({ item }) => {
+    const { name, img, quantity, description, sold, supplierEmail, supplierName, price, _id } = item
     return (
-        <div>
-            hello
+        <div className='intentoryCardContainer'>
+            <img src={img} alt="" />
+            <p className='pdName'>{name}</p>
+            <p className='pdPrice'>${price}</p>
+            <p className='stock'>Stock: {quantity}</p>
+            <p className='supplier'>Supplier: {supplierName}</p>
+            <p className='des'>{description}</p>
+            <br />
+            <button className='detailBtn'>Details</button>
         </div>
     );
 };
